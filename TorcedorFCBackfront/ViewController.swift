dcf4fb621bd9e81c6bf110a03e5024978528b0ff
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-08-29 11:13:16 -0300

    Inicialização do projeto em grupo e criação da LauchSccrenn. Já adicionada a logo também.

---
+++ TorcedorFCBackfront/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TorcedorFCBackfront
+//
+//  Created by Marcelo Falcao Costa Filho on 29/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
